6fb994f430b532bb90996ad8691d97bd95c0c665
queyrouzec <queyrouzec@gmail.com>
2019-05-06 11:29:41 -0500

    text to speech mock up

--- apiHelpers.js
+++ apiHelpers.js
@@ -1,4 +1,10 @@
 const axios = require('axios');
+const fs = require('fs');
+const util = require('util');
+const textToSpeech = require('@google-cloud/text-to-speech');
+const AWS = require('aws-sdk');
+const path = require('path');
+const client = new textToSpeech.TextToSpeechClient();
 
 
 /**
@@ -28,6 +34,49 @@ const googleTextToSpeech = () => {
 }
 
 
+const googleTextToSpeech = (word) => {
+
+  const mp3Promise = new Promise((res, rej) => {
+    const request = {
+      input: { text: word },
+      voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
+      audioConfig: { audioEncoding: 'MP3' },
+    };
+  
+    // Performs the Text-to-Speech request
+    const [response] = await client.synthesizeSpeech(request);
+    // Write the binary audio content to a local file
+    const writeFile = util.promisify(fs.writeFile);
+    await writeFile(`${word}.mp3`, response.audioContent, 'binary');
+    console.log(`Audio content written to file: ${word}.mp3`);
+  
+    const filePath = `./${word}.mp3`;
+  
+    const params = {
+      Bucket: 'vocapp-bucket',
+      Body: fs.createReadStream(filePath),
+      Key: "words/" + path.basename(filePath),
+      ACL: 'public-read'
+    };
+  
+    S3.upload(params, function (err, data) {
+      if (err) {
+        console.log("Error", err);
+        res.send(err);
+      }
+      if (data) {
+        fs.unlinkSync(`${word}.mp3`);
+        console.log("Uploaded in:", data.Location);
+        res.send('uploaded at: ' + data.Location);
+      }
+    });
+  })
+  
+  return Promise.resolve(mp3Promise)
+}
+
+
 module.exports = {
   googleTranslate,
+  googleTextToSpeech, 
 }
--- routes/text-to-speech.js
+++ routes/text-to-speech.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
-const util = require('util');
-const textToSpeech = require('@google-cloud/text-to-speech');
-const AWS = require('aws-sdk');
-const path = require('path');
-const client = new textToSpeech.TextToSpeechClient();
+// const fs = require('fs');
+// const util = require('util');
+// const textToSpeech = require('@google-cloud/text-to-speech');
+// const AWS = require('aws-sdk');
+// const path = require('path');
+// const client = new textToSpeech.TextToSpeechClient();
+const { googleTextToSpeech } = require('../apiHelpers');
 
 
 //Configuring AWS environment
@@ -25,46 +26,51 @@ router.get('/:word', (req, res) => {
 
   const word = req.params.word;
 
-  async function main() {
+  googleTextToSpeech(word)
+    .then(mp3Path => {
+      res.send(mp3Path);
+    })
 
-    const request = {
-      input: { text: word },
-      voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
-      audioConfig: { audioEncoding: 'MP3' },
-    };
+  // async function main() {
 
-    // Performs the Text-to-Speech request
-    const [response] = await client.synthesizeSpeech(request);
-    // Write the binary audio content to a local file
-    const writeFile = util.promisify(fs.writeFile);
-    await writeFile(`${word}.mp3`, response.audioContent, 'binary');
-    console.log(`Audio content written to file: ${word}.mp3`);
+  //   const request = {
+  //     input: { text: word },
+  //     voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
+  //     audioConfig: { audioEncoding: 'MP3' },
+  //   };
 
-    const filePath = `./${word}.mp3`;
+  //   // Performs the Text-to-Speech request
+  //   const [response] = await client.synthesizeSpeech(request);
+  //   // Write the binary audio content to a local file
+  //   const writeFile = util.promisify(fs.writeFile);
+  //   await writeFile(`${word}.mp3`, response.audioContent, 'binary');
+  //   console.log(`Audio content written to file: ${word}.mp3`);
 
-    const params = {
-      Bucket: 'vocapp-bucket',
-      Body: fs.createReadStream(filePath),
-      Key: "words/" + path.basename(filePath),
-      ACL: 'public-read'
-    };
+  //   const filePath = `./${word}.mp3`;
 
-    S3.upload(params, function (err, data) {
-      if (err) {
-        console.log("Error", err);
-        res.send(err);
-      }
-      if (data) {
-        fs.unlinkSync(`${word}.mp3`);
-        console.log("Uploaded in:", data.Location);
-        res.send('uploaded at: ' + data.Location);
-      }
-    });
+  //   const params = {
+  //     Bucket: 'vocapp-bucket',
+  //     Body: fs.createReadStream(filePath),
+  //     Key: "words/" + path.basename(filePath),
+  //     ACL: 'public-read'
+  //   };
+
+  //   S3.upload(params, function (err, data) {
+  //     if (err) {
+  //       console.log("Error", err);
+  //       res.send(err);
+  //     }
+  //     if (data) {
+  //       fs.unlinkSync(`${word}.mp3`);
+  //       console.log("Uploaded in:", data.Location);
+  //       res.send('uploaded at: ' + data.Location);
+  //     }
+  //   });
 
 
-  }
+  // }
 
-  main();
+  // main();
 
 })
 
