d757a4600d51da5f64a484375b709e1567cf4f32
queyrouzec <queyrouzec@gmail.com>
2019-05-10 09:56:24 -0500

    speech to text is sending back undefined but the request go through

--- apiHelpers.js
+++ apiHelpers.js
@@ -5,6 +5,8 @@ const textToSpeech = require('@google-cloud/text-to-speech');
 const AWS = require('aws-sdk');
 const path = require('path');
 const client = new textToSpeech.TextToSpeechClient();
+const speech = require('@google-cloud/speech');
+const btoa = require('btoa');
 
 
 //Configuring AWS environment
@@ -99,6 +101,59 @@ const googleTextToSpeech = (word, languageCode = 'en') => {
   
   return Promise.resolve(mp3Promise)
 }
+
+
+
+const googleSpeechToText = async (binary) => {
+
+  // Imports the Google Cloud client library
+  // const speech = require('@google-cloud/speech');
+  // const fs = require('fs');
+  
+  // Creates a client
+  const client = new speech.SpeechClient();
+
+  // The name of the audio file to transcribe
+  // const fileName = `./resources/audio.raw`;
+
+  // Reads a local audio file and converts it to base64
+  // const file = fs.readFileSync(fileName);
+  // const audioBytes = file.toString('base64');
+  const audioBytes = btoa(binary);
+
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    content: audioBytes,
+  };
+  const config = {
+    encoding: 'LINEAR16',
+    sampleRateHertz: 16000,
+    languageCode: 'en-US',
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
+
+  // Detects speech in the audio file
+  const [response] = await client.recognize(request);
+  const transcription = response.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n');
+  console.log(`Transcription: ${transcription}`);
+}
+
+
+(async() => {
+  let yeetus = await fs.readFileSync('./word.mp3')
+  googleSpeechToText(yeetus)
+    .then(lmao => {
+      console.log(lmao)
+    })
+    .catch(err => {
+      console.error(err);
+    })
+})()
 
 
 module.exports = {
